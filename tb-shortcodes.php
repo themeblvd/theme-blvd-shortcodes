1b30a3baa6f47a638504ff4f25ef04de6752807a
Jason Bobich <jasonbobich@gmail.com>
2015-02-15 15:58:16 -0900

    Plugin license copyright date

--- tb-shortcodes.php
+++ tb-shortcodes.php
@@ -7,7 +7,7 @@ Author: Theme Blvd
 Author URI: http://themeblvd.com
 License: GPL2
 
-    Copyright 2013  Theme Blvd
+    Copyright 2015  Theme Blvd
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2,
